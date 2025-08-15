28a4e72ea29b5494d3ff10a9b3762599032a3ddb
Mark <208412620+Markyriott@users.noreply.github.com>
2025-08-15 14:47:51 -0400

    Configured lualine some more on Haephestus

--- .config/nvim/lua/plugins/dracula.lua
+++ .config/nvim/lua/plugins/dracula.lua
@@ -2,5 +2,6 @@ require("dracula").setup({
 
     show_end_of_buffer = true,
     transparent_bg = true,
+    lualine_bg_color = "#44475a",
     italic_comment = true
 })
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,6 +1,180 @@
-require("lualine").setup({
-    options = {
-        icons_enabled = true,
-        theme = "dracula-nvim",
+-- Eviline config for lualine
+-- Author: shadmansaleh
+-- Credit: glepnir
+local lualine = require('lualine')
+
+-- Color table for highlights
+-- stylua: ignore
+local colors = require("dracula").colors()
+
+local conditions = {
+  buffer_not_empty = function()
+    return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
+  end,
+  hide_in_width = function()
+    return vim.fn.winwidth(0) > 80
+  end,
+  check_git_workspace = function()
+    local filepath = vim.fn.expand('%:p:h')
+    local gitdir = vim.fn.finddir('.git', filepath .. ';')
+    return gitdir and #gitdir > 0 and #gitdir < #filepath
+  end,
+}
+
+local transparentbg = nil
+
+-- Config
+local config = {
+  options = {
+    component_separators = '',
+    section_separators = '',
+    theme = {
+      normal = { c = { fg = colors.fg, bg = colors.black} } ,
+      inactive = { c = { fg = colors.fg, bg = colors.black} },
+    },
+  },
+ sections = {
+    -- these are to remove the defaults
+    lualine_a = {},
+    lualine_b = {},
+    lualine_y = {},
+    lualine_z = {},
+    -- These will be filled later
+    lualine_c = {},
+    lualine_x = {},
+  },
+  inactive_sections = {
+    -- these are to remove the defaults
+    lualine_a = {},
+    lualine_b = {},
+    lualine_y = {},
+    lualine_z = {},
+    lualine_c = {},
+    lualine_x = {},
+  },
+}
+
+local mode_map = {
+      ['NORMAL'] = 'N',
+      ['O-PENDING'] = 'N?',
+      ['INSERT'] = 'I',
+      ['VISUAL'] = 'V',
+      ['V-BLOCK'] = 'VB',
+      ['V-LINE'] = 'VL',
+      ['V-REPLACE'] = 'VR',
+      ['REPLACE'] = 'R',
+      ['COMMAND'] = '!',
+      ['SHELL'] = 'SH',
+      ['TERMINAL'] = 'T',
+      ['EX'] = 'X',
+      ['S-BLOCK'] = 'SB',
+      ['S-LINE'] = 'SL',
+      ['SELECT'] = 'S',
+      ['CONFIRM'] = 'Y?',
+      ['MORE'] = 'M',
+}
+
+-- Inserts a component in lualine_c at left section
+local function ins_left(component)
+  table.insert(config.sections.lualine_c, component)
+end
+
+-- Inserts a component in lualine_x at right section
+local function ins_right(component)
+  table.insert(config.sections.lualine_x, component)
+end
+
+-- left items
+ins_left {
+  function()
+    return '▊'
+  end,
+  color = { fg = colors.bright_yellow }, -- Sets highlighting of component
+  padding = { left = 0, right = 1 }, -- We don't need space before this
+}
+
+ins_left {
+  -- mode component
+  'mode',
+  fmt = function(s) return mode_map[s] or s end,
+  gui = 'bold',
+  color = function()
+    -- auto change color according to neovims mode
+    local mode_color = {
+      n = colors.yellow,
+      i = colors.green,
+      v = colors.blue,
+      [''] = colors.blue,
+      V = colors.blue,
+      c = colors.magenta,
+      no = colors.red,
+      s = colors.orange,
+      S = colors.orange,
+      [''] = colors.orange,
+      ic = colors.yellow,
+      R = colors.violet,
+      Rv = colors.violet,
+      cv = colors.red,
+      ce = colors.red,
+      r = colors.cyan,
+      rm = colors.cyan,
+      ['r?'] = colors.cyan,
+      ['!'] = colors.red,
+      t = colors.red,
     }
-})
+    return { fg = mode_color[vim.fn.mode()] or colors.blue }
+  end,
+  -- padding = { right = 1 },
+}
+
+ins_left {
+  'branch',
+  cond = conditions.check_git_workspace,
+  icon = '',
+  color = { fg = colors.white, gui="bold"},
+}
+
+ins_left {
+  'filename',
+  cond = conditions.buffer_not_empty,
+  color = { fg = colors.fg, gui = 'bold' },
+}
+
+-- ins_left { 'location' }
+
+-- ins_left { 'progress', color = { fg = colors.fg, gui = 'bold' } }
+ins_left {
+  'diagnostics',
+  sources = { 'nvim_diagnostic' },
+  symbols = { error = ' ', warn = ' ', info = ' ' },
+  diagnostics_color = {
+    error = { fg = colors.red },
+    warn = { fg = colors.yellow },
+    info = { fg = colors.cyan },
+  },
+}
+
+
+-- Add components to right sections
+ins_right {
+  'diff',
+  -- Is it me or the symbol for modified us really weird
+  symbols = { added = ' ', modified = '󰝤 ', removed = ' ' },
+  diff_color = {
+    added = { fg = colors.green },
+    modified = { fg = colors.orange },
+    removed = { fg = colors.red },
+  },
+  cond = conditions.hide_in_width,
+}
+
+ins_right {
+  function()
+    return '▊'
+  end,
+  color = { fg = colors.bright_yellow},
+  padding = { left = 1 },
+}
+
+-- Now don't forget to initialize lualine
+lualine.setup(config)
