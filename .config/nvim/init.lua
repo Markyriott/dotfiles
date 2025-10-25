4720a37a1bafb7c0f35f51152a6ca9db205743c0
Mark <208412620+Markyriott@users.noreply.github.com>
2025-10-25 16:02:51 -0400

    added nvim-lint

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,12 +7,15 @@ Plug('nvim-tree/nvim-tree.lua')
 Plug('Mofiqul/dracula.nvim')
 Plug('folke/which-key.nvim')
 Plug('nvim-lualine/lualine.nvim')
+Plug('mfussenegger/nvim-lint')
 
 vim.call("plug#end")
 
 require("config.options")
 require("config.keymaps")
+--require("config.autocmd")
 
+--require("plugins.nvim-lint")
 require("plugins.nvim-tree")
 require("plugins.dracula")
 require("plugins.lualine")
---
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,5 @@
+require('lint').linters_by_ft = {
+    lua = {'luac'},
+    markdown = {'vale'},
+    javascript = {'eslint_d'},
+}
